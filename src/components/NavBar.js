80c4ab2df878417c0cc2e0a461cdc718536a607d
Sediqa <sadat.hadid2000@gmail.com>
2023-07-18 19:36:59 +0000

    AAdd basic Navbar structure in place

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="md" fixed='top'>
+        <Container>
+            <Navbar.Brand href="#home">Pythonista</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link1">Create Post</Nav.Link>
+                <Nav.Link href="#link2">Meetup</Nav.Link>
+                <NavDropdown title="Profile" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Sign Out</NavDropdown.Item>
+                </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
