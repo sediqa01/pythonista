a42fca4374fef37f29a5fbe51be843b50aa6d097
Sediqa <sadat.hadid2000@gmail.com>
2023-07-19 22:30:18 +0000

    style the NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,33 @@
 import React from 'react'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import styles from "../styles/NavBar.module.css"
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed='top'>
+    <Navbar className={styles.NavBar} expand="md" fixed='top'>
         <Container>
-            <Navbar.Brand>
-              <i className="fa-solid fa-code" aria-hidden="true"></i>
-              <span> Pythonista</span>
+            <Navbar.Brand className={styles.NavBrand}>
+              <i class="fa-solid fa-code" aria-hidden="true"></i>
+              <span>Pythonista</span>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                  <Nav.Link>
-                  <i class="fa-solid fa-house"></i> Home
+                  <Nav.Link className={styles.NavLink}>
+                  <i class="fa-solid fa-house"></i>Home
                   </Nav.Link>
-                  {/* <Nav.Link>
+                  {/* <Nav.Link className={styles.NavLink}>
                   <i class="fa-solid fa-square-plus"></i> Create Post
                   </Nav.Link>
-                  <Nav.Link>
+                  <Nav.LinkclassName={styles.NavLink} >
                   <i class="fa-solid fa-calendar-days"></i> Meetup
                   </Nav.Link> */}
-                  <NavDropdown
-                      title={
-                        <span>
-                            <i className="fas fa-user-alt ml-5"></i>
-                        </span>
-                      }
-                      id="basic-nav-dropdown">
-                      <NavDropdown.Item>
-                      <i class="fa-solid fa-right-to-bracket"></i> Sign In
-                      </NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item>
-                      <i class="fa-solid fa-user-plus"></i> Sign Up
-                      </NavDropdown.Item>
-                  </NavDropdown>
+                   <Nav.Link className={styles.NavLink}>
+                   <i class="fa-solid fa-right-to-bracket"></i>Sign In
+                  </Nav.Link>
+                  <Nav.Link className={styles.NavLink}>
+                  <i class="fa-solid fa-user-plus"></i>Sign Up
+                  </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
