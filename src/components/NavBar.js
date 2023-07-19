b48a16c736af9ed1ed32d56a146eac6f61a8bdc9
Sediqa <sadat.hadid2000@gmail.com>
2023-07-19 17:06:48 +0000

    Add Google font and general style for site

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,31 +11,31 @@ const NavBar = () => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link>
-                <i class="fa-solid fa-house"></i> Home
-                </Nav.Link>
-                {/* <Nav.Link>
-                <i class="fa-solid fa-square-plus"></i> Create Post
-                </Nav.Link>
-                <Nav.Link>
-                <i class="fa-solid fa-calendar-days"></i> Meetup
-                </Nav.Link> */}
-                <NavDropdown
-                    title={
-                      <span>
-                          <i className="fas fa-user-alt ml-5"></i>
-                      </span>
-                    }
-                    id="basic-nav-dropdown">
-                    <NavDropdown.Item>
-                    <i class="fa-solid fa-right-to-bracket"></i> Sign In
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item>
-                    <i class="fa-solid fa-user-plus"></i> Sign Up
-                    </NavDropdown.Item>
-                </NavDropdown>
+                <Nav className="ml-auto text-left">
+                  <Nav.Link>
+                  <i class="fa-solid fa-house"></i> Home
+                  </Nav.Link>
+                  {/* <Nav.Link>
+                  <i class="fa-solid fa-square-plus"></i> Create Post
+                  </Nav.Link>
+                  <Nav.Link>
+                  <i class="fa-solid fa-calendar-days"></i> Meetup
+                  </Nav.Link> */}
+                  <NavDropdown
+                      title={
+                        <span>
+                            <i className="fas fa-user-alt ml-5"></i>
+                        </span>
+                      }
+                      id="basic-nav-dropdown">
+                      <NavDropdown.Item>
+                      <i class="fa-solid fa-right-to-bracket"></i> Sign In
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item>
+                      <i class="fa-solid fa-user-plus"></i> Sign Up
+                      </NavDropdown.Item>
+                  </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Container>
