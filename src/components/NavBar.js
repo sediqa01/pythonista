4a9cef23b00d1d5318f9cb25fee5874430bbf8e4
Sediqa <sadat.hadid2000@gmail.com>
2023-07-21 23:34:48 +0000

    Add navbar toggle hook to close burger menu.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,84 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from 'react-router-dom'
 import styles from "../styles/NavBar.module.css"
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import Avatar from './Avatar';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const setCurrentUser = useSetCurrentUser();
+
+  // Variables to toggle open and closed mobile navbar burger menu
+  const [toggleNavBar, setToggleNavBar] = useState(false);
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const createPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="fa-solid fa-square-plus"></i>Create Post
+    </NavLink>
+  );
+
+  const loggedInIcons = (
+    <>
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/events">
+      <i className="fa-solid fa-calendar-days"></i>Meetup
+    </NavLink>
+    <NavDropdown 
+        title={
+          <span>
+              <i className="fas fa-user-alt ml-5"></i>
+          </span>
+        }
+        id="basic-nav-dropdown" 
+      >
+      <NavDropdown.Item 
+        className={styles.dropdownItems}
+        as={Link} 
+        to={`/profiles/${currentUser?.profile_id}`}
+        onClick={() => {
+          setToggleNavBar(!toggleNavBar);
+        }}
+      >
+      <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavDropdown.Item>
+      <NavDropdown.Divider />
+      <NavDropdown.Item 
+        className={styles.dropdownItems}
+        as={Link} 
+        to="/" 
+        onClick={handleSignOut}
+      >
+      <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavDropdown.Item>
+    </NavDropdown>
+    </>
+  )
+  // const profileDropdownIcons = (
+  //   <>
+    
+  //   </>
+  // )
   const loggedOutIcons = (
     <>
     <NavLink
@@ -27,7 +98,11 @@ const NavBar = () => {
   </>
   )
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed='top'>
+    <Navbar
+     className={styles.NavBar}
+      expand="md"
+      fixed='top'
+      expanded={expanded}>
         <Container>
             <NavLink to="/">
             <Navbar.Brand className={styles.NavBrand}>
@@ -35,7 +110,10 @@ const NavBar = () => {
               <span>Pythonista</span>
             </Navbar.Brand>
             </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle
+              ref={ref}
+              onClick={() => setExpanded(!expanded)} 
+              aria-controls="basic-navbar-nav"  />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto text-left">
                   <NavLink
@@ -46,12 +124,10 @@ const NavBar = () => {
                     >
                   <i className="fa-solid fa-house"></i>Home
                   </NavLink>
-                  {/* <NavLink className={styles.NavLink}>
-                  <i className="fa-solid fa-square-plus"></i> Create Post
-                  </NavLink>
-                  <NavLink className={styles.NavLink} >
-                  <i className="fa-solid fa-calendar-days"></i> Meetup
-                  </NavLink> */}
+                  {currentUser && createPostIcon}
+                  
+                  {/* {currentUser && profileDropdownIcons} */}
+                 
                   {currentUser ? loggedInIcons : loggedOutIcons}
                 </Nav>
             </Navbar.Collapse>
