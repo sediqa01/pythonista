e364452284567f25d536ce7b917133834ed4887e
Sediqa <sadat.hadid2000@gmail.com>
2023-07-25 20:19:28 +0000

    Add logic for fetching post data

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import {Col, Row, Container } from "react-bootstrap";
 
+
+import { axiosReq } from "../../api/axiosDefaults";
+import { useParams } from "react-router-dom";
 import appStyles from "../../App.module.css";
 
 function PostPage() {
+
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
