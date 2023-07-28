e4a83b0cf89214a4e12e9742a8684b2b0a327d00
Sediqa <sadat.hadid2000@gmail.com>
2023-07-28 13:30:40 +0000

    Add handleDelete funtion in comment dropdown

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -52,7 +52,12 @@ function PostPage() {
           ) : null}
           {comments.results.length ? (
             comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
             ))
           ) : currentUser ? (
             <span>Be the first to comment!</span>
